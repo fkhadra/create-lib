b9dc3a86c005021ec801e0ee26e01f478822a076
Fadi Khadra <fdkhadra@gmail.com>
2016-10-12 15:54:51 +0200

    Start to work on git and npm integration

--- src/index.js
+++ src/index.js
@@ -3,10 +3,6 @@
 'use strict';
 
 const yargs = require('yargs');
-
-yargs.usage('Usage: $0 projectName')
-  .demand(1)
-  .argv;
 /*
  * Require if yargs
  * */
@@ -17,6 +13,23 @@ const isWin = /^win/.test(process.platform);
 const projectName = argv._[0];
 const config = require('./config').make(projectName);
 const utils = require('./utils');
+
+yargs
+  .usage('Usage: $0 <command> [options]')
+  .command('lib-name','Create the lib folder with the given name')
+  .example('$0 foobar --git-init=true --npm-init=true', 'Same as $0 foobar')
+  .default('git-init', true, 'Init a new git repository')
+  .default('npm-init', true, 'Run npm init')
+  .default('verbose', 0, 'Verbose')
+  .boolean(['git-init', 'npm-init', 'v'])
+  .demand(1)
+  .help('h')
+  .alias('h', 'help')
+  .count('verbose')
+  .alias('v', 'verbose')
+  .argv;
+
+
 
 console.log('Try to create directory');
 
